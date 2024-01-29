605c730d7b1a3ad7072139aa89966ade46b129ee
Dhruv Chandak <dhruvchandak5@gmail.com>
2024-01-29 16:52:21 +0530

    Added Fonts

--- client/src/components/AboutUs/AboutUs.js
+++ client/src/components/AboutUs/AboutUs.js
@@ -1,15 +1,15 @@
 import React from "react";
-import "./AboutUs.css";
 import img1 from "../../assets/Crowd1.jpg";
 import Ramsha1 from "../../assets/Ramsha1.png";
 import img3 from "../../assets/Crowd2.jpg";
-import AboutUsImg from "../../assets/AboutUs.png";
 
 const AboutUs = () => {
   return (
     <div className="">
       <div className="flex items-center justify-center my-16">
-        <img src={AboutUsImg} alt="About Us"></img>
+        <h1 className="font-pricedown text-white lg:text-8xl text-3xl AboutUs">
+          About Us
+        </h1>
       </div>
       <div className="flex flex-row justify-center">
         <img alt="aboutUS" src={img1}></img>
