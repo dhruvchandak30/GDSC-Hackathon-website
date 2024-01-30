7e9f7535c934780599dadfd1066fad21440f2c3b
Dhruv Chandak <dhruvchandak5@gmail.com>
2024-01-30 15:21:32 +0530

    Added PS

---
+++ client/src/components/PS/PS.js
@@ -0,0 +1,25 @@
+import React from "react";
+import PSCard from "./PSCard";
+import PSData from "./PSData";
+
+const PS = () => {
+  return (
+    <div className="my-8">
+      <div className="font-pricedown text-white text-7xl text-center my-32">
+        Problem Statements
+      </div>
+      <div className="flex lg:flex-row  lg:flex-wrap flex-col justify-evenly items-center">
+        {PSData.map((item) => (
+          <PSCard
+            key={item.id}
+            img={item.img}
+            title={item.title}
+            desc={item.desc}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default PS;
