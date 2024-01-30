3b79ee46db8299070229b60db4206401d7e144ad
Dhruv Chandak <dhruvchandak5@gmail.com>
2024-01-30 14:10:24 +0530

    Made Responsiveness

--- client/src/components/Events/Event.js
+++ client/src/components/Events/Event.js
@@ -20,27 +20,26 @@ const Event = () => {
       setEventsImage();
     }
   };
-  const HideDescriptionHandler = () => {
-    const Container = document.getElementById("EventContainer1");
-    // console.log(Container.style)
-    if (window.innerWidth < 900) {
-      if (Container.style.display === "none") {
-        console.log("Block");
-        Container.style.display = "block";
-      } else if (Container.style.display === "block") {
-        console.log("None");
-        Container.style.display = "none";
-      } else {
-      }
+  const [EventImageVisibility, setEventImage] = useState(true);
+
+  const ShowHideDescriptionHandler = () => {
+    if (!EventImageVisibility) {
+      document.getElementById("EventImagee").style.filter = "brightness(100%)";
+      document.getElementById("EventContainer1").style.display = "none";
+      setEventImage(!EventImageVisibility);
+      return;
+    } else {
+      setEventImage(!EventImageVisibility);
+      document.getElementById("EventContainer1").style.display = "block";
+      document.getElementById("EventImagee").style.filter = "brightness(50%)";
+      return;
     }
   };
 
   return (
     <div>
       <div className="flex justify-center align-middle py-16">
-        <h1 className="font-pricedown lg:text-8xl text-3xl  text-white ">
-          Events
-        </h1>
+        <h1 className="font-pricedown text-7xl  text-white ">Events</h1>
       </div>
       <div className="flex justify-center">
         <div className="flex flex-col p-4 gap-4 justify-evenly">
@@ -60,21 +59,27 @@ const Event = () => {
         </div>
 
         <div className="flex items-cente relative">
-          <div className="EventImage" onClick={HideDescriptionHandler}>
+          <div
+            className=""
+            onMouseEnter={ShowHideDescriptionHandler}
+            // onMouseLeave={ShowHideDescriptionHandler}
+            onClick={ShowHideDescriptionHandler}
+          >
             <img
               src={eventsImage}
-              className="items-center flex justify-between"
+              id="EventImagee"
+              className="items-center flex justify-between "
               alt="Events"
             />
           </div>
           <div
             id="EventContainer1"
-            className="text-white EventContainer ml-[10%] mt-[10%] absolute"
+            className="text-white  ml-[10%] lg:mt-[10%] absolute"
           >
-            <h1 className="lg:text-5xl text-3xl my-2 font-array">
+            <h1 className="lg:text-5xl text-3xl lg:my-2 font-array">
               {EventTitle}
             </h1>
-            <p className="text-white lg:text-xl text-sm lg:w-2/3">
+            <p className="text-white lg:text-xl text-[12px] lg:w-2/3 p-2 ">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi
               non quis exercitationem culpa nesciunt nihil aut nostrum explicabo
               reprehenderit optio amet ab temporibus asperiores quasi
--- client/src/components/Events/EventTitleCard.js
+++ client/src/components/Events/EventTitleCard.js
@@ -4,7 +4,7 @@ const EventTitleCard = ({ text, onClick }) => {
   return (
     <div
       onClick={onClick}
-      className="text-white  border-white border-2 hover:text-black cursor-pointer hover:bg-white lg:text-3xl text-xl py-4 text-bold lg:w-64 w-32 px-2  flex text-center items-center -skew-x-12 font-array"
+      className="text-white  border-white border-2 hover:text-black cursor-pointer hover:bg-white lg:text-3xl text-xl lg:py-4 py-1 text-bold lg:w-64 w-32 px-2  flex text-center items-center -skew-x-12 font-array"
     >
       {text}
     </div>
