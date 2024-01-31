6163f8f4e7177dfd877348ec37e108a2bd10ed7f
Krishna <krishnakhattri24@gmail.com>
2024-01-31 16:46:06 +0530

    Changes

--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -18,17 +18,22 @@ const Hero = () => {
     <div className="homebg h-screen ">
       {status ? <HamburgerNavbar /> : <Navbar />}
       <div className="relative h-[85%]">
-        <div className="bg1 lg:h-[350px] h-[500px] lg:w-3/4 w-[90%] absolute top-1/2 left-[50%] transform -translate-x-1/2 -translate-y-1/2">
-          <div className="bg2 lg:h-[355px] h-[505px] w-full absolute top-[47%] lg:top-[45%] left-[48%] transform -translate-x-1/2 -translate-y-1/2 flex lg:flex-row flex-col justify-center lg:items-end pb-20 gap-y-8 items-center">
-            <div className="lg:absolute lg:top-[10%] lg:left-[15%]">
-              <img src={heading} alt="" />
+        <div className="bg1 sm:h-[350px] h-[300px] lg:w-3/4 w-[90%] absolute top-1/2 left-[50%] transform -translate-x-1/2 -translate-y-1/2">
+          <div className="bg2 sm:h-[355px] h-[305px] w-full absolute top-[47%] lg:top-[45%] left-[48%] transform -translate-x-1/2 -translate-y-1/2 flex sm:flex-row flex-col justify-center lg:items-end pb-8 gap-y-4 items-center">
+            <div className="text-white sm:absolute  sm:top-[10%] sm:left-[12%]">
+              {/* <img src={heading} alt="" /> */}
+              <h1 className="font-pricedown text-7xl lg:text-9xl">GAME</h1>
+              <h1 className="font-pricedown text-7xl lg:text-9xl relative">
+                codex <span className=" text-3xl sm:text-5xl absolute bottom-0 sm:bottom-2 -right-12 am:-right-14">1.0</span>
+              </h1>
             </div>
-            <div className="lg:absolute lg:top-[50%] lg:-translate-y-1/2 lg:right-[10%]">
-              <img src={title} alt="" />
+            <div className="text-[#EC576D] sm:absolute sm:top-[50%] sm:-translate-y-1/2 sm:right-[10%]">
+              {/* <img src={title} alt="" /> */}
+              <h1 className="font-array text-3xl lg:text-5xl">Dev Hackathon <br /> By <br /> GDSC, LNMIIT</h1>
             </div>
           </div>
         </div>
-        <div className=" absolute lg:bottom-14 bottom-20 lg:right-48 transform -translate-x-1/2 right-1/2  w-32">
+        <div className=" absolute sm:bottom-14 bottom-20 sm:right-[22%] transform translate-x-1/2 w-32 right-[30%]">
           <img src={register} alt="" className="cursor-pointer" />{" "}
         </div>
       </div>
