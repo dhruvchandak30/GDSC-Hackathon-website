b3d0c7d3cbbbbe4bbbadbaaacdd91d5e401e919a
Krishna <krishnakhattri24@gmail.com>
2024-02-03 16:41:20 +0530

    animations hero

--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -5,6 +5,7 @@ import title from "../../assets/homeLine.svg";
 import register from "../../assets/register.svg";
 import "./style.css";
 import HamburgerNavbar from "../Navbar/HamburgerNavbar";
+import { motion } from "framer-motion";
 
 const Hero = () => {
   const [wid, setWid] = useState(window.innerWidth);
@@ -14,29 +15,92 @@ const Hero = () => {
     if (wid < 1020) setStatus(true);
     else setStatus(false);
   }, [wid]);
+
   return (
     <div className="homebg h-screen ">
       {status ? <HamburgerNavbar /> : <Navbar />}
       <div className="relative h-[85%]">
-        <div className="bg1 sm:h-[350px] h-[300px] lg:w-3/4 w-[70%] mx-auto absolute top-1/2 left-[50%] transform -translate-x-1/2 -translate-y-1/2">
-          <div className="bg2 sm:h-[355px] h-[305px] w-full absolute top-[47%] lg:top-[45%] left-[48%] transform -translate-x-1/2 -translate-y-1/2 flex sm:flex-row flex-col justify-center lg:items-end pb-8 gap-y-4 items-center">
+        <motion.div
+          initial={{
+            x: "-100%",
+            y: "-50%",
+          }}
+          animate={{
+            x: "-50%",
+            y: "-50%",
+          }}
+          transition={{ duration: 0.2 }}
+          className="bg1 sm:h-[350px] h-[300px] lg:w-3/4 w-[70%] mx-auto absolute top-1/2 left-[50%] transform -translate-x-1/2 -translate-y-1/2"
+        >
+          <motion.div
+            initial={{
+              x: "100%",
+              y: "-50%",
+            }}
+            animate={{
+              x: "-50%",
+              y: "-50%",
+            }}
+            transition={{ duration: 0.2 }}
+            className="bg2 sm:h-[355px] h-[305px] w-full absolute top-[47%] lg:top-[45%] left-[48%] transform -translate-x-1/2 -translate-y-1/2 flex sm:flex-row flex-col justify-center lg:items-end pb-8 gap-y-4 items-center"
+          >
             <div className="text-white sm:absolute  sm:top-[10%] sm:left-[12%]">
               {/* <img src={heading} alt="" /> */}
-              <h1 className="font-pricedown text-7xl lg:text-9xl textborder mb-4"> GAME</h1>
-              <h1 className="font-pricedown text-7xl lg:text-9xl relative textborder">
-                codex <span className=" text-3xl sm:text-5xl absolute bottom-0 sm:bottom-2 -right-12 am:-right-14">1.0</span>
-              </h1>
+              <motion.h1
+                initial={{
+                  scale: 10,
+                  opacity: 0,
+                }}
+                animate={{
+                  scale: 1,
+                  opacity: 1,
+                }}
+                transition={{ duration: 0.4, delay: 0.2 }}
+                className="font-pricedown text-7xl lg:text-9xl textborder mb-4"
+              >
+                {" "}
+                GAME
+              </motion.h1>
+              <motion.h1
+                initial={{
+                  scale: 10,
+                  opacity: 0,
+                }}
+                animate={{
+                  scale: 1,
+                  opacity: 1,
+                }}
+                transition={{ duration: 0.4, delay: 0.2 }}
+                className="font-pricedown text-7xl lg:text-9xl relative textborder"
+              >
+                codex{" "}
+                <span className=" text-3xl sm:text-5xl absolute bottom-0 sm:bottom-2 -right-12 am:-right-14">
+                  1.0
+                </span>
+              </motion.h1>
             </div>
             <div className="text-[#EC576D] sm:absolute sm:top-[50%] sm:-translate-y-1/2 sm:right-[10%]">
               {/* <img src={title} alt="" /> */}
-              <h1 className="font-array text-3xl lg:text-5xl">Dev Hackathon <br /> By <br /> GDSC, LNMIIT</h1>
+              <h1 className="font-array text-3xl lg:text-5xl">
+                Dev Hackathon <br /> By <br /> GDSC, LNMIIT
+              </h1>
             </div>
-          </div>
-          <div className=" absolute sm:bottom-[-20%] bottom-[-20%] sm:right-[18%] transform translate-x-1/2 w-32 right-[30%]">
-          <img src={register} alt="" className="cursor-pointer" />{" "}
-        </div>
-        </div>
-        
+          </motion.div>
+          <motion.div
+            initial={{
+              scale: 0,
+              opacity: 0,
+            }}
+            animate={{
+              scale: 1,
+              opacity: 1,
+            }}
+            transition={{ duration: 0.4, delay: 0.4 }}
+            className=" absolute sm:bottom-[-20%] bottom-[-20%] sm:right-[18%] transform translate-x-1/2 w-32 right-[30%]"
+          >
+            <img src={register} alt="" className="cursor-pointer" />{" "}
+          </motion.div>
+        </motion.div>
       </div>
     </div>
   );
