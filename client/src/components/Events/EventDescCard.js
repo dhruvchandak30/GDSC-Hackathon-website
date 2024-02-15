cc2c8df840b4e495a254b70fa6fa986d2940e1bb
Dhruv Chandak <dhruvchandak5@gmail.com>
2024-02-15 11:39:41 +0530

    Done All changes

--- client/src/components/Events/EventDescCard.js
+++ client/src/components/Events/EventDescCard.js
@@ -4,7 +4,7 @@ const EventDescCard = ({ title, text }) => {
   return (
     <div>
       <h1 className="lg:text-5xl text-3xl lg:my-2 font-array">{title}</h1>
-      <p className="text-white lg:text-xl text-[12px] lg:w-2/3 lg:p-2  ">
+      <p className="text-white lg:text-xl text-[12px] lg:w-2/3 lg:p-2  font-instagram">
         {text}
       </p>
     </div>
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -2,47 +2,47 @@ import React, { useState } from "react";
 import logo from "../../assets/logo.svg";
 import "./navbar.css";
 import { motion } from "framer-motion";
-import {useNavigate} from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
-  const [bg, setBg] = useState([false, false, false, false,false]);
+  const [bg, setBg] = useState([false, false, false, false, false]);
   const nav = useNavigate();
 
   const hoverHandler = (e) => {
-    const temp = [false, false, false, false,false];
+    const temp = [false, false, false, false, false];
     temp[e] = true;
     setBg(temp);
   };
 
   const hoverOut = () => {
-    setBg([false, false, false, false,false]);
+    setBg([false, false, false, false, false]);
   };
 
-  const goToHome=()=>{
+  const goToHome = () => {
     nav("/");
     return true;
-  }
+  };
 
   const scrollAboutUs = async () => {
-    const response=await goToHome();
-    if(response){
-    var elem = document.getElementById("AboutUsid");
-    elem.scrollIntoView({ behavior: "smooth" });
+    const response = await goToHome();
+    if (response) {
+      var elem = document.getElementById("AboutUsid");
+      elem.scrollIntoView({ behavior: "smooth" });
     }
   };
   const scrollTimeLine = async () => {
-    const response=await goToHome();
-    if(response){
-    var elem = document.getElementById("TimeLineid");
-    elem.scrollIntoView({ behavior: "smooth" });
+    const response = await goToHome();
+    if (response) {
+      var elem = document.getElementById("TimeLineid");
+      elem.scrollIntoView({ behavior: "smooth" });
     }
   };
 
   const scrollPS = async () => {
-    const response=await goToHome();
-    if(response){
-    var elem = document.getElementById("PSId");
-    elem.scrollIntoView({ behavior: "smooth" });
+    const response = await goToHome();
+    if (response) {
+      var elem = document.getElementById("PSId");
+      elem.scrollIntoView({ behavior: "smooth" });
     }
   };
 
@@ -54,44 +54,52 @@ const Navbar = () => {
         </div>
         <ul className="flex gap-x-6 ">
           <li
-          onClick={()=>nav("/")}
+            onClick={() => nav("/")}
             className={`text-center w-fit px-8 h-[30px] cursor-pointer ${
               bg[0] ? "back" : ""
             }`}
             onMouseEnter={() => hoverHandler(0)}
             onMouseLeave={hoverOut}
           >
-            <h5 className="text-[#FFFFFF] heading cursor-pointer w-full">
+            <h5 className="text-[#FFFFFF] heading cursor-pointer w-full font-instagram">
               Home
             </h5>
           </li>
           <li
-          onClick={()=>nav("/gamejam")}
+            onClick={() => nav("/gamejam")}
             className={`text-center w-fit px-8 h-[30px] cursor-pointer  ${
               bg[1] ? "back" : ""
             }`}
             onMouseEnter={() => hoverHandler(1)}
             onMouseLeave={hoverOut}
           >
-            <h5 className="text-[#FFFFFF] heading cursor-pointer w-full">
+            <h5 className="text-[#FFFFFF] heading cursor-pointer w-full font-instagram">
               GameJam
             </h5>
           </li>
           <li
             onClick={scrollAboutUs}
-            className={`text-center px-6 h-[30px] cursor-pointer ${bg[2] ? "back" : ""}`}
+            className={`text-center px-6 h-[30px] cursor-pointer ${
+              bg[2] ? "back" : ""
+            }`}
             onMouseEnter={() => hoverHandler(2)}
             onMouseLeave={hoverOut}
           >
-            <h5 className="text-[#FFFFFF] heading cursor-pointer">About Us</h5>
+            <h5 className="text-[#FFFFFF] heading cursor-pointer font-instagram">
+              About Us
+            </h5>
           </li>
           <li
             onClick={scrollTimeLine}
-            className={`text-center px-6  h-[30px] cursor-pointer ${bg[3] ? "back" : ""}`}
+            className={`text-center px-6  h-[30px] cursor-pointer ${
+              bg[3] ? "back" : ""
+            }`}
             onMouseEnter={() => hoverHandler(3)}
             onMouseLeave={hoverOut}
           >
-            <h5 className="text-[#FFFFFF] heading cursor-pointer">Timeline</h5>
+            <h5 className="text-[#FFFFFF] heading cursor-pointer font-instagram">
+              Timeline
+            </h5>
           </li>
           <li
             onClick={scrollPS}
@@ -101,7 +109,7 @@ const Navbar = () => {
             onMouseEnter={() => hoverHandler(4)}
             onMouseLeave={hoverOut}
           >
-            <h5 className="text-[#FFFFFF] heading cursor-pointer">
+            <h5 className="text-[#FFFFFF] heading cursor-pointer  font-instagram">
               Problem Statements
             </h5>
           </li>
